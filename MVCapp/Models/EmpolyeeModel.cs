dc884dd00b786f896779f2f3308c3adb5a15db8e
Jehad1965 <55837928+Jehad1965@users.noreply.github.com>
2020-10-14 22:16:02 +0100

    Add project files.

---
+++ MVCapp/Models/EmpolyeeModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapp.Models
+{
+    public class EmpolyeeModel
+    {
+        [Display(Name ="Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "You should Enter a valid Employee ID")]
+        public int EmployeeID { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage =" You must provide your first name")]
+        public String FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = " You must provide your Last name")]
+        public String LastName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = " You must provide your Email Address")]
+        public String EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email Address")]
+        [Compare("EmailAddress",ErrorMessage =" The Emails Not Match")]
+        public String ConfirmEmail { get; set; }
+
+        [DataType(DataType.Password )]
+        [StringLength(100,MinimumLength = 8,ErrorMessage ="Enter a long Enough Password")]
+        public String Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Your Password and Your Confirmation Not Match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
